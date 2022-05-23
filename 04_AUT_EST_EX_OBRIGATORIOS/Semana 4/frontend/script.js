f0f3961c28bab64b02887cf9673bc06c4662f6ec
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2022-05-23 15:53:27 -0300

    update repository

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 4/frontend/script.js
@@ -0,0 +1,3 @@
+﻿function mostrarInfos(){
+  document.getElementById("dados").innerHTML = "<strong>Endereco:</strong> Rua M.M.D.C, 80 - Apto. 917 - Sao Paulo - SP <br /><strong>Telefone: </strong> (15) 99716-5061 <br /><strong>Email:</strong>matias.r.gabriela@gmail.com"; 
+}
