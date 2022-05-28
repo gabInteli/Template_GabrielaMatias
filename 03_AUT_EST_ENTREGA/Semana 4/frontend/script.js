0610d00b5e32aa50c28b201ef76f34041c14ecac
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2022-05-28 12:41:29 -0300

    add jQuery

--- 03_AUT_EST_ENTREGA/Semana 4/frontend/script.js
+++ 03_AUT_EST_ENTREGA/Semana 4/frontend/script.js
@@ -1,3 +1,5 @@
 ﻿function mostrarInfos(){
-  document.getElementById("dados").innerHTML = "<strong>Endereco:</strong> Rua M.M.D.C, 80 - Apto. 917 - Sao Paulo - SP <br /><strong>Telefone: </strong> (15) 99716-5061 <br /><strong>Email:</strong>matias.r.gabriela@gmail.com"; 
+  var text ="#dados"
+
+  $(text).append(`<strong>Endereco:</strong> Rua M.M.D.C, 80 - Apto. 917 - Sao Paulo - SP <br /><strong>Telefone: </strong> (15) 99716-5061 <br /><strong>Email:</strong>matias.r.gabriela@gmail.com`); 
 }
